3d48b665e3c1da56120587079a765dbeeb9852fd
kgricour <kevin@newton.so>
2023-10-09 17:24:04 +0200

    update with new polyfact version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import { Footer } from "@/components/Footer";
 import { Loader } from "@/components/Loader";
 import ChatWrapper from "@/components/ChatWrapper";
 import config from "@/config";
+import { PolyfactProvider } from "polyfact/hooks";
 
 function App() {
   const [isBrowser, setIsBrowser] = useState(false);
@@ -20,7 +21,16 @@ function App() {
       style={{ backgroundColor: config.chat.botMessageBackgroundColor }}
     >
       <Header title={config.chat.botName} {...config.header} />
-      <div className="content">{isBrowser ? <ChatWrapper /> : <Loader />}</div>
+      <div className="content">
+        {isBrowser ? (
+          <PolyfactProvider project="chat_07b29de">
+            <ChatWrapper />
+          </PolyfactProvider>
+        ) : (
+          <Loader />
+        )}
+      </div>
+
       <Footer name={config.chat.botName} {...config.footer} />
     </div>
   );
--- src/components/ChatWrapper/index.tsx
+++ src/components/ChatWrapper/index.tsx
@@ -1,27 +1,21 @@
-import { usePolyfact, type Chat } from "polyfact";
+import { usePolyfact } from "polyfact/hooks";
+import type { Chat as ChatType } from "polyfact";
 import { useState, useEffect } from "react";
 import ChatUI from "@polyfact/chat";
-import config from "@/config";
+import { Loader } from "../Loader";
 
 const ChatWrapper = () => {
-  const { polyfact, login, loading } = usePolyfact({
-    project: "7b29de50-8ce4-4449-8382-115eee716f76",
-  });
-  const [chat, setChat] = useState<Chat>();
+  const { auth: {login, status}, utils: {Chat} } = usePolyfact();
+  const [chat, setChat] = useState<ChatType>();
+
 
   useEffect(() => {
-    if (polyfact) {
-      setChat(
-        new polyfact.Chat({
-          systemPromptId:
-            config.chat.promptId || "feea76f3-af40-4df9-be47-ad073d504615",
-          autoMemory: true,
-        })
-      );
+    if (!chat) {
+      setChat(new Chat({ autoMemory: true }));
     }
-  }, [polyfact]);
+  }, [status]);
 
-  if (login) {
+  if (login && status === "unauthenticated") {
     return (
       <button
         onClick={() => login({ provider: "github" })}
@@ -32,8 +26,31 @@ const ChatWrapper = () => {
     );
   }
 
-  if (chat) {
-    return <ChatUI chat={chat} {...config.chat} />;
+  if (status === "loading") {
+    return <Loader />;
+  }
+
+  if (chat && status === "authenticated") {
+    return (
+      <ChatUI 
+          chat={chat}
+          chatBackgroundColor="rgba(111, 111, 111, 0.1)"
+          chatTextColor="#2D3748"
+          inputBackgroundColor="#E2E8F0"
+          inputColor="#2D3748"
+          placeholderTextColor="#A0AEC0"
+          botMessageColor="#2D3748"
+          botMessageBackgroundColor="rgba(220, 242, 247)"
+          userMessageColor="#E2E8F0"
+          userMessageBackgroundColor="#4A5568"
+          botName="ChatBot"
+          buttonBackgroundColor="#4A5568"
+          buttonBorderColor="#2D3748"
+          buttonBorderWidth="1px"
+          dotsColor="#A0AEC0"
+      />
+  );
+  
   }
 
   return null;
