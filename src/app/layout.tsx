2f2b63c7c0419649a5f72ecd60e88e41b5240ff4
kgricour <kevin@newton.so>
2023-11-14 13:46:50 +0100

    refact booilerplate with polyfire

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,37 @@
-import './globals.css'
+"use client";
+
+import { PolyfireProvider } from "polyfire-js/hooks";
+import { Header } from "@/components/Header";
+import { Footer } from "@/components/Footer";
+
+import "./globals.css";
+
+const BOT_NAME = process.env.NEXT_PUBLIC_CHATBOT_NAME || "Chatbot";
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
       <head />
-      <body>{children}</body>
+
+      <body className="app-container">
+        <PolyfireProvider
+          // Need to add the project alias in the .env file
+          project={process.env.NEXT_PUBLIC_POLYFIRE_PROJECT || ""}
+        >
+          <Header
+            title={BOT_NAME}
+            logo="./logo.svg" // to replace with your own logo replace the logo.svg file in the public folder
+            bgColor="#2D3748"
+            textColor="#E2E8F0"
+          />
+          <div className="content">{children}</div>
+          <Footer name={BOT_NAME} bgColor="#2D3748" textColor="#E2E8F0" />
+        </PolyfireProvider>
+      </body>
     </html>
-  )
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,40 +1,96 @@
 "use client";
-
+import { usePolyfire } from "polyfire-js/hooks";
 import { useState, useEffect } from "react";
-import { Header } from "@/components/Header";
-import { Footer } from "@/components/Footer";
-import { Loader } from "@/components/Loader";
-import ChatWrapper from "@/components/ChatWrapper";
-import config from "@/config";
-import { PolyfactProvider } from "polyfact/hooks";
 
-function App() {
-  const [isBrowser, setIsBrowser] = useState(false);
+import { Chat as ChatType } from "polyfire-js";
+import { Loader } from "@/components/Loader";
+
+import ChatUI from "@polyfact/chat";
+import ColorPickerWrapper from "@/components/Colors";
+
+const BOT_NAME = process.env.NEXT_PUBLIC_CHATBOT_NAME || "Chatbot";
+
+const defaultColors = {
+  chatBackgroundColor: "rgba(111, 111, 111, 0.1)",
+  chatTextColor: "#2D3748",
+  inputBackgroundColor: "#E2E8F0",
+  inputColor: "#2D3748",
+  placeholderTextColor: "#A0AEC0",
+  botMessageColor: "#2D3748",
+  botMessageBackgroundColor: "rgba(220, 242, 247)",
+  userMessageColor: "#E2E8F0",
+  userMessageBackgroundColor: "#4A5568",
+  buttonBackgroundColor: "#4A5568",
+  buttonBorderColor: "#2D3748",
+  dotsColor: "#A0AEC0",
+};
+
+const Chat = () => {
+  const {
+    auth: { login, status },
+    utils: { Chat },
+  } = usePolyfire();
+  const [chat, setChat] = useState<ChatType>();
 
   useEffect(() => {
-    setIsBrowser(typeof window !== "undefined");
-  }, []);
+    if (!chat) {
+      setChat(new Chat({ autoMemory: true }));
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [status]);
 
-  return (
-    <div
-      className="app-container"
-      style={{ backgroundColor: config.chat.botMessageBackgroundColor }}
-    >
-      <Header title={config.chat.botName} {...config.header} />
-      <div className="content">
-        {isBrowser ? (
-          <PolyfactProvider project="chat_07b29de">
-            {/* TODO: Replace with your project ID */}
-            <ChatWrapper />
-          </PolyfactProvider>
-        ) : (
-          <Loader />
-        )}
-      </div>
+  if (login && status === "unauthenticated") {
+    return (
+      <button
+        onClick={() => login({ provider: "github" })}
+        className="login-btn"
+      >
+        Login with Github
+      </button>
+    );
+  }
 
-      <Footer name={config.chat.botName} {...config.footer} />
-    </div>
-  );
-}
+  if (status === "loading") {
+    return <Loader />;
+  }
 
-export default App;
+  if (chat && status === "authenticated") {
+    /////////////////////////////////////////////////////
+    // ColorPickerWrapper is only used for the demo    //
+    // After you have chosen your colors,              //
+    // replace the defaultColors and delete this code  //
+    /////////////////////////////////////////////////////
+
+    if (process.env.NODE_ENV !== "production") {
+      return (
+        <ColorPickerWrapper initialColors={defaultColors}>
+          {({ colors }) => {
+            console.log("colorss", colors);
+            return (
+              <ChatUI
+                chat={chat}
+                botName={BOT_NAME}
+                buttonBorderWidth="1px"
+                {...colors}
+              />
+            );
+          }}
+        </ColorPickerWrapper>
+      );
+    }
+    ////////////////////////////////////////////////
+
+    return (
+      <ChatUI
+        chat={chat}
+        botName={BOT_NAME}
+        buttonBorderWidth="1px"
+        {...defaultColors}
+      />
+    );
+  }
+
+  return null;
+};
+
+export default Chat;
